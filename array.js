3e24dc69ed3df613f09d42168ea736afa512556d
bhakta <dashlucky248@gmail.com>
2020-12-06 14:29:40 +0530

    modified files

---
+++ array.js
@@ -0,0 +1,63 @@
+// // Filter && Find && FindIndex
+
+// const ages = [16, 32, 56, 87, 97, 5, 1];
+
+// function FilterAge(age) {
+//   return age >= 18;
+// }
+// const res = ages.filter(FilterAge);
+// // console.log(res);
+// const reslt = ages.find(FilterAge);
+// // console.log(reslt);
+
+// const Fres = ages.findIndex(FilterAge);
+// // console.log(Fres);
+
+// var people = [
+//   { name: "Sandeep", empId: 0, age: 23, orgId: 1 },
+//   { name: "Arjun", empId: 1, age: 25, orgId: 2 },
+//   { name: "Nanha", empId: 3, age: 22, orgId: 1 },
+//   { name: "Nandu", empId: 4, age: 53, orgId: 3 },
+// ];
+// var organizations = [
+//   { name: "Google", id: 1 },
+//   { name: "Apple", id: 2},
+//   { name: "Microsoft", id: 3 },
+// ];
+
+
+// var organization = [
+//   { name: "Sandeep", empId: 0, age: 23, orgId: 1 },
+//   { name: "Arjun", empId: 1, age: 25, orgId: 2 },
+//   { name: "Nanha", empId: 3, age: 22, orgId: 1 },
+//   { name: "Nandu", empId: 4, age: 53, orgId: 3 },
+// ];
+
+// function org(people, organization) {
+//   console.log(organization)
+//     if( people.orgId === organization.orgId)
+//     {
+//       return people
+//     }
+// }
+//  const name = people.filter(org)
+// console.log(name);
+
+
+
+// void function xobin_eg(){
+//   var msg = function (){
+//     console.log("hello world");
+//   }
+//   msg();
+// }();
+
+// var a = new Array(4)
+// for (var i = 0; i<a.length; i++){
+//   a[i] = i*2
+//   console.log(a[i]);
+  
+// }
+
+var myBook = new String("Perl")
+console.log(myBook.repeat(2));
